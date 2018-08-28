96110e148b560507e9e01902675f2459913ed919
unknown <mathilde.jean@depinfonancy.net>
2018-08-28 20:51:56 +0200

    removing unused code

--- app/src/main/java/com/test/endroits/home/ui/HomeActivity.kt
+++ app/src/main/java/com/test/endroits/home/ui/HomeActivity.kt
@@ -1,17 +1,9 @@
 package com.test.endroits.home.ui
 
-import android.content.Context
-import android.content.Intent
 import android.support.v4.app.Fragment
 import com.test.endroits.infrastructure.base.SingleFragmentActivity
 
 class HomeActivity: SingleFragmentActivity(){
-
-    companion object {
-        fun getLaunchIntent(context: Context): Intent {
-            return Intent(context, HomeActivity::class.java)
-        }
-    }
 
     override val fragment: Fragment
         get() = HomeFragment.newInstance()
